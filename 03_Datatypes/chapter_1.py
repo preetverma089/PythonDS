c748de3e371bf823d125c626ee0b9548de3275b0
Sahil <sahilsangwan5577@gmail.com>
2026-09-02 02:26:40 +0530

    Initial changes

--- 03_Datatypes/chapter_1.py
+++ 03_Datatypes/chapter_1.py
@@ -8,8 +8,8 @@ sugar_amount = 2
 print(f"Initial_Sugar_Amount : {sugar_amount}");
 sugar_amount = 12
 print(f"Second_Sugar_Amount : {sugar_amount}");
-print("id of 2: {id(2)}")
-print(f"Id of 2 :{id(2)}")
+print("id of 2: {id(2)}") # agar f nhi lagaya to id of 2: {id(2)} print hoga na ki uska address bcz ye isko string me treat kr rha h na ki variable ke value ko print kr rha h
+print(f"Id of 2 :{id(2)}")#id ka matlab hota hai identity, identifier, address of object in memory jiska mtlb h ki har object ka ek unique address hota h memory me jisko hum id() function se check kr skte h
 print(f"Id of 12 :{id(12)}")
 
 # everything in python is object
