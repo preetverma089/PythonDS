d8b38a831e091a302aaf4909774f148741dc6073
Sahil <sahilsangwan5577@gmail.com>
2026-09-18 15:29:25 +0530

    added extra info

--- 08_comprehensions/02_Set_compreh.py
+++ 08_comprehensions/02_Set_compreh.py
@@ -1,4 +1,8 @@
 # set comprehensions list comprehension ki trh bilkul same h but square bracket ki jga curly bracket ayenge
+#how set internally works:-
+# Ram ke ander ek set bna or vo set ek hollow space hota hai or us hollow space me hum jo bhi value save krte hai uski ek hash value create hoti hai or ek baar me hum sirf ek value ki hi hash value  store krta hai or jab dusri value aaegi to uski alag hash value store hoti hai  naaki ek hi hash me saari value store ho
+# or jab bhi aap koi duplicate value dekhte ho to uski bhi hash value same hogi to python bolta hai ki pehle se hi ek aisi similar hash value exist krti hai so thats why duplicate value aapki stor nhi hogi
+
 # {expression for item of iterable condition}
 
 # favourite_chais = [
